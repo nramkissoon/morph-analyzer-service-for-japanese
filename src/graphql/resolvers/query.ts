80a66cf182bcf3394867a7f1549a6c8d4df8caef
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-02-16 20:49:48 -0500

    add time logging

--- src/graphql/resolvers/query.ts
+++ src/graphql/resolvers/query.ts
@@ -2,17 +2,20 @@ import { mecabParseSync } from './../../mecab/mecab';
 import { Query, Arg, Resolver } from 'type-graphql';
 import { MecabOutput } from '../schema';
 import { logger } from './../../utils/logger';
+import { performance } from 'perf_hooks';
 
 @Resolver()
 export class QueryResolver {
     @Query(() => MecabOutput, { nullable: false })
     tokenize(@Arg('text', () => String, { nullable: false }) text: string): MecabOutput {
-        logger.log('info', `new tokenize query received.`);
+        const t0 = performance.now();
+        logger.log('info', `query: tokenize: text arg length: ${text.length}`);
         const mecabOutput = mecabParseSync(text);
         if (mecabOutput.error) {
             const error = mecabOutput.error;
             logger.log('error', `${error.name}: ${error.message}`);
         }
+        logger.log('info', `query: tokenize: resolver execution time: ${performance.now() - t0}`);
         return mecabParseSync(text);
     }
 }
