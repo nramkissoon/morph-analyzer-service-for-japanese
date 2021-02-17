a5cc2eb1f4173eca20890562327ac48f74d7548b
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-02-16 20:06:20 -0500

    add s3 stream logging

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,38 @@
 import { format, transports, createLogger } from 'winston';
+import { S3StreamLogger } from 's3-streamlogger';
+import { config } from 'dotenv';
+
+config(); // config environment variables
+
+const createTransports = (isProductionEnvironment: boolean) => {
+    const prod_log_bucket_name = process.env.S3_LOG_BUCKET_NAME_PRODUCTION;
+    const dev_log_bucket_name = process.env.S3_LOG_BUCKET_NAME_DEVELOPMENT;
+    const aws_access_key = process.env.AWS_ACCESS_KEY_ID;
+    const aws_secret_key = process.env.AWS_SECRET_ACCESS_KEY;
+    if (isProductionEnvironment) {
+        const s3_stream = new S3StreamLogger({
+            bucket: prod_log_bucket_name,
+            access_key_id: aws_access_key,
+            secret_access_key: aws_secret_key,
+        });
+        return [new transports.Stream({ stream: s3_stream })];
+    } else {
+        const s3_stream = new S3StreamLogger({
+            bucket: dev_log_bucket_name,
+            access_key_id: aws_access_key,
+            secret_access_key: aws_secret_key,
+        });
+        return [new transports.Stream({ stream: s3_stream }), new transports.Console({})];
+    }
+};
 
 const { combine, timestamp, printf } = format;
 const logFormat = printf(({ level, message, timestamp }) => {
     return `${timestamp} ${level}: ${message}`;
 });
 
-const devTransports = [new transports.Console({})];
-const prodTransports = [new transports.File({ filename: 'logs/logs.log' })];
-
 export const logger = createLogger({
     format: combine(timestamp(), logFormat),
     defaultMeta: { service: 'morphological-analyzer-service' },
-    transports: process.env.NODE_ENV === 'development' ? devTransports : prodTransports,
+    transports: createTransports(process.env.NODE_ENV === 'production'),
 });
